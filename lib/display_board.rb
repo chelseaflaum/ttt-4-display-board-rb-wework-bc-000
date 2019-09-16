6a6931d3ad491e2982a81ff76ff6a3477186e6f0
Chelsea Flaum <chelsea.flaum@wework.com>
2019-09-16 10:59:43 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,12 @@
 board = [" "," "," "," "," "," "," "," "," "]
 
+def display_board(board)
+
+puts "    |   |   "
+puts "-----------"
+puts "    |   |   "
+puts "-----------"
+puts "    |   |   "
+end
+
+puts display_board(board)
